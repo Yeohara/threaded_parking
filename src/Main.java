e2a95af0be1e11392b3a924970a249b42c32489a
Yeohara <kirokirishiki@gmail.com>
2020-11-24 12:19:42 +0300

    Added new exception for incorrect input of max_queue_size

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     int num_of_parking_spaces = scanner.nextInt();
     int max_queue_size = scanner.nextInt();
+    if (max_queue_size < 1) {
+        throw new IllegalArgumentException("Размер очереди должен быть больше 0. Нет длины очереди - нет дороги на парковку.");
+    }
     int interval_in = scanner.nextInt();
     int interval_out = scanner.nextInt();
     if (interval_in <= 0 || interval_out <= 0) {
-        throw new IllegalArgumentException("Интервалы должны быть больше 0");
+        throw new IllegalArgumentException("Интервалы должны быть больше 0.");
     }
     new Parking(num_of_parking_spaces, max_queue_size, interval_in, interval_out);
     }
